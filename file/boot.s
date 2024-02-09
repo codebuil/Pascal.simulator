af7c8743044ed3b9713098ed1027b7a7d46c8363
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2024-02-09 07:05:31 +0000

    Add files via upload

---
+++ file/boot.s
@@ -0,0 +1,34 @@
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384 # 16 KiB
+stack_top:
+
+
+
+.global _starts
+
+.global screens
+.global fpc_do_exit
+.global FPC_LIB_EXIT
+.global PC_LIB_EXIT
+.global fpc_initializeunits
+.global fpc_libinitializeunits
+.global THREADVARLIST_$SYSTEM$indirect
+.global INIT$_$SYSTEM
+.extern main
+_starts:
+	
+	call main
+FPC_LIB_EXIT:
+	ret
+haltss:
+    jmp haltss
+    INIT$_$SYSTEM:
+screens:
+THREADVARLIST_$SYSTEM$indirect:
+
+PC_LIB_EXIT:
+fpc_do_exit:
+fpc_initializeunits:
+ret
---
+++ file/kernel.pas
@@ -0,0 +1,17 @@
+program kernel;
+
+var
+        i: Integer;
+        scr: PChar = PChar($b8000);       
+
+begin
+
+        for i := 0 to 2000 do
+        begin
+               scr[i*2] := #65;
+               scr[i*2+1] := #23;
+        end;
+               
+               
+end.
+
